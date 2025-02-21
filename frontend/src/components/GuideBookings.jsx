2cc10f1ff8dab409686476032b445ea672dba994
sunadi amarasinghe <sunadiamarasinghe@gmail.com>
2025-02-21 21:50:35 +0530

    Add GuideBookings component

---
+++ frontend/src/components/GuideBookings.jsx
@@ -0,0 +1,13 @@
+// GuideBookings.jsx
+import React from "react";
+
+const GuideBookings = () => {
+    return (
+        <div>
+            <h1>Guide Bookings</h1>
+            <p>This is the guide bookings page.</p>
+        </div>
+    );
+};
+
+export default GuideBookings;
